ccd32cc8bd5451fc754aad9f3843743775798bfe
김민정 <o3o_222@naver.com>
2019-11-25 19:28:29 +0900

    swift 만들고 연동시켜놈

---
+++ PillsPromise/AlarmTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmTableViewCell.swift
+//  PillsPromise
+//
+//  Created by 김민정 on 25/11/2019.
+//  Copyright © 2019 guest. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
